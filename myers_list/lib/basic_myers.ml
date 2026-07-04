350ac40ff24d33d54188beaf6bdb0e23fc58d6a9
malvito <malvito@outlook.pt>
2026-07-04 19:54:02 +0100

    Correções de labels e tipos nas funções Basic

--- myers_list/lib/basic_myers.ml
+++ myers_list/lib/basic_myers.ml
@@ -102,27 +102,29 @@ type 'a t = Nil | Cell of 'a cell
 let empty = Nil
 let return x = Cell (init 0 x)
 
-let is_empty l = function
+let is_empty = function
     | Nil -> false
     | Cell _ -> true
 
-let hd l = function
+let hd = function
     | Nil -> invalid_arg "Empty List"
     | Cell {value} -> value
 
-let tl l = function
+let tl = function
     | Nil -> invalid_arg "Empty List"
-    | Cell {next} -> next
+    | Cell {next} -> Cell next
 
-let front l = function
+let front = function
     | Nil -> None
-    | Cell c -> Some (c.value, c.next)
+    | Cell c -> Some (c.value, Cell c.next)
 
-let front_exn l = function
+let front_exn = function
     | Nil -> invalid_arg "Empty List"
-    | Cell c -> (c.value, c.next) 
+    | Cell c -> (c.value, Cell c.next) 
 
-let length l = l.length + 1
+let length = function
+    | Nil -> 0
+    | Cell c -> c.length + 1
 
 let get l i = match l with
     | Nil -> None
@@ -130,7 +132,7 @@ let get l i = match l with
         if i > c.length || i < 0 then
             None
         else
-            Some (index 0 c i)
+            Some (index 0 c i).value
 
 let get_exn l i = match l with
     | Nil -> invalid_arg "Empty List"
@@ -138,7 +140,7 @@ let get_exn l i = match l with
         if i > c.length || i < 0 then
             invalid_arg "Invalid Index"
         else
-            index 0 c i
+            (index 0 c i).value
 
 let cons x l = match l with
     | Nil -> return x
@@ -146,29 +148,29 @@ let cons x l = match l with
 
 let cons' xs x = cons x xs
 
-let rec fold f acc l = match l with
+let rec fold ~f ~x:acc l = match l with
     | Nil -> acc
     | Cell c -> match c with
         | {length = 0} -> f acc c.value
-        | _ -> fold f (f acc c.value) (Cell (c.next))
+        | _ -> fold ~f ~x:(f acc c.value) (Cell (c.next))
 
-let rec fold_rev f acc l = match l with
+let rec fold_rev ~f ~x:acc l = match l with
     | Nil -> acc
     | Cell c -> match c with
         | {length = 0} -> f acc c.value
         | _ -> 
-            let acc' = fold_rev f acc (Cell (c.next)) in
+            let acc' = fold_rev ~f ~x:acc (Cell (c.next)) in
             f acc' c.value
 
-let map f l =
+let map ~f l =
     let func acc x = cons (f x) acc in
-    fold_rev func Nil l
+    fold_rev ~f:func ~x:Nil l
 
 (* TODO: tentar refazer com fold_rev *)
-let mapi i f l = match l with
+let mapi ~f l = match l with
     | Nil -> Nil
     | Cell c ->
-        let rec go (idx: int) (acc: 'a list) (f: int -> 'a -> 'b) (curr: 'a cell) = match curr with 
+        let rec go (idx: int) (acc: 'b list) (f: int -> 'a -> 'b) (curr: 'a cell) = match curr with 
             | {length = 0} -> (f idx curr.value) :: acc
             | _ -> go (idx + 1) ((f idx curr.value) :: acc) f curr.next in
         List.fold_left cons' Nil (go 0 [] f c)
@@ -216,28 +218,28 @@ let get_and_remove_exn l i = match l with
             let info = go [] (c.length - i) c in
             ((snd info).value, List.fold_left cons' (Cell ((snd info).next)) (fst info))
 
-let append l1 l2 = fold_rev cons' l2 l1
+let append l1 l2 = fold_rev ~f:cons' ~x:l2 l1
 
-let filter f l = 
+let filter ~f l = 
     let func acc x = if (f x) then (cons x acc) else acc in
-    fold_rev func Nil l
+    fold_rev ~f:func ~x:Nil l
 
-let filter_map f l = 
+let filter_map ~f l = 
     let func acc x = match (f x) with
         | Some y -> (cons y acc)
         | None -> acc in
-    fold_rev func Nil l
+    fold_rev ~f:func ~x:Nil l
 
 let flat_map f l =
     let func acc x = append (f x) acc in
-    fold_rev func Nil l
+    fold_rev ~f:func ~x:Nil l
 
 (* TODO: refazer com append *)
-let flatten lists =  fold_rev (fold_rev cons') Nil lists
+let flatten lists = fold_rev ~f:(fun acc l -> fold_rev ~f:cons' ~x:acc l) ~x:Nil lists
 
 let app funs l =
-    let func acc f = fold_rev (fun acc x -> cons (f x) acc) acc l in
-    fold_rev func Nil funs
+    let func acc f = fold_rev ~f:(fun acc x -> cons (f x) acc) ~x:acc l in
+    fold_rev ~f:func ~x:Nil funs
 
 let rec take_ n l acc = match l with
     | Nil -> Nil
@@ -259,7 +261,7 @@ let rec take n l =
 let rec take_while_ f l acc = match l with
     | Nil -> Nil
     | Cell c -> 
-        if !(f c.value) then
+        if not (f c.value) then
             acc     (* TODO: ver se ainda tenho q juntar o c.value *)
         else if c.length = 0 then
             cons c.value acc
@@ -267,7 +269,7 @@ let rec take_while_ f l acc = match l with
             let acc' = take_while_ f (Cell (c.next)) acc in
             cons c.value acc'
 
-let rec take_while f l = take_while_ f l Nil
+let rec take_while ~f l = take_while_ f l Nil
 
 let rec drop n l = match l with
     | Nil -> Nil
@@ -279,15 +281,15 @@ let rec drop n l = match l with
         else
             drop (n-1) (Cell (c.next))
 
-let rec drop_while f l = match l with
+let rec drop_while ~f l = match l with
     | Nil -> Nil
     | Cell c ->    
-        if !(f c.value) then 
+        if not (f c.value) then 
             l
         else if c.length = 0 then
             Nil
         else
-            drop_while f (Cell (c.next))
+            drop_while ~f (Cell (c.next))
 
 let rec take_drop_ n l acc = match l with
     | Nil -> (Nil, l)
@@ -306,11 +308,11 @@ let rec take_drop n l =
     else
         take_drop_ n l Nil
 
-let rec iter f l = match l with
+let rec iter ~f l = match l with
     | Nil -> ()
     | Cell c -> match c with
         | {length = 0} -> f c.value
-        | _ -> f c.value; iter f (Cell (c.next))
+        | _ -> f c.value; iter ~f (Cell (c.next))
 
 let rec iteri_ i f l = match l with
     | Nil -> ()
@@ -318,15 +320,15 @@ let rec iteri_ i f l = match l with
         | {length = 0} -> f i c.value
         | _ -> f i c.value; iteri_ (i+1) f (Cell (c.next))
 
-let iteri = iteri_ 0
+let iteri ~f l = iteri_ 0 f l
 
-let rev_map f l =
+let rev_map ~f l =
     let func acc x = cons (f x) acc in
-    fold func Nil l
+    fold ~f:func ~x:Nil l
 
-let rev = fold cons' Nil
+let rev l = fold ~f:cons' ~x:Nil l
 
-let equal eq l1 l2 = match (l1, l2) with
+let equal ~eq l1 l2 = match (l1, l2) with
     | (Nil, Nil) -> true
     | (Nil, Cell _)
     | (Cell _, Nil) -> false
@@ -335,7 +337,7 @@ let equal eq l1 l2 = match (l1, l2) with
             false
         else
             let rec equal' eq c1 c2 =
-                if !(eq c1.value c2.value) then
+                if not (eq c1.value c2.value) then
                     false
                 else if c1.length = 0 then
                     true
@@ -343,7 +345,7 @@ let equal eq l1 l2 = match (l1, l2) with
                     equal' eq c1.next c2.next in
             equal' eq c1 c2
 
-let compare cmp l1 l2 = match (l1, l2) with
+let compare ~cmp l1 l2 = match (l1, l2) with
     | (Nil, Nil) -> 0
     | (Nil, Cell _) -> 1
     | (Cell _, Nil) -> -1
@@ -400,9 +402,9 @@ let add_list l1 l2 = List.fold_right cons l2 l1 (* TODO: talvez usar fold_left p
 
 let of_list l = List.fold_right cons l Nil (* TODO: talvez usar fold_left pois é TR *)
 
-let to_list l = fold_rev (fun acc x -> x :: acc) [] l
+let to_list l = fold_rev ~f:(fun acc x -> x :: acc) ~x:[] l
 
-let of_list f l = List.fold_right (fun x acc -> cons (f x) acc) l Nil (* TODO: talvez usar fold_left pois é TR *)
+let of_list_map ~f l = List.fold_right (fun x acc -> cons (f x) acc) l Nil (* TODO: talvez usar fold_left pois é TR *)
 
 let of_array a = Array.fold_right (fun x acc -> cons x acc) a Nil
 
@@ -412,13 +414,16 @@ let to_array l = match l with
     | Nil ->  [||]
     | Cell c -> 
         let a = Array.make (c.length + 1) c.value in
-        iteri (fun i x -> Array.set a i x) (Cell c.next);
+        iteri ~f:(fun i x -> Array.set a i x) (Cell c.next);
         a
+
+type 'a iter = ('a -> unit) -> unit
+type 'a gen = unit -> 'a option
 
 let add_iter l it = 
     let res = ref Nil in
     it (fun x -> res := cons x !res);
-    fold cons' l !res
+    fold ~f:cons' ~x:l !res
 
 let of_iter it = 
     let l = ref Nil in
@@ -426,7 +431,7 @@ let of_iter it =
     rev !l
 
 (* TODO: rever função *)
-let to_iter l yield = iter yield l 
+let to_iter l yield = iter ~f:yield l 
 
 (* TODO: rever função *)
 let add_gen l g =
@@ -438,7 +443,7 @@ let add_gen l g =
             gen_iter f g in
     let res = ref Nil in
     gen_iter (fun x -> res := cons x !res) g;
-    fold (fun acc x -> cons x acc) l !res
+    fold ~f:(fun acc x -> cons x acc) ~x:l !res
 
 let of_gen g = add_gen Nil g
 
@@ -465,7 +470,7 @@ let to_gen l = match l with
 module Infix = struct
     let ( @+ ) = cons
     let ( >>= ) l f = flat_map f l
-    let ( >|= ) l f = map f l
+    let ( >|= ) l f = map ~f l
     let ( <*> ) = app
     let ( -- ) = range
     let ( --^ ) = range_excl_
@@ -473,10 +478,12 @@ end
 
 include Infix
 
+type 'a printer = Format.formatter -> 'a -> unit
+
 (* TODO: rever função *)
 let pp ?(pp_sep = fun fmt () -> Format.fprintf fmt ",@ ") pp_item fmt l =
     let first = ref true in
-    iter (fun x ->
+    iter ~f:(fun x ->
         if !first then
             first := false
         else
